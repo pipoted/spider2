cd1ed7ffdbef5bd8dc7ab28286766ea28c4010fa
pipoted <32660879+pipoted@users.noreply.github.com>
2018-11-24 21:53:21 +0800

    selenium

--- learn/python3/day07/selenium_demo02.py
+++ learn/python3/day07/selenium_demo02.py
@@ -1,16 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome()
 driver.get('https://www.baidu.com/')
 
-
-input_tag = driver.find_element_by_id('kw')
-submit_tag = driver.find_element_by_id('su')
-
-actions = ActionChains(driver)
-actions.move_to_element(input_tag)
-actions.send_keys_to_element(input_tag, 'python')
-actions.move_to_element(submit_tag)
-actions.click(submit_tag)
-actions.perform()
+submit_btn =driver.find_element_by_id('su')
+print(type(submit_btn))
+# print(submit_btn.get_attribute('value')
+driver.save_screenshot('baidu.png')
+driver.quit()
